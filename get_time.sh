9c154b9341858808192ac551755163c9781acbcb
Mcdull <1042762344@qq.com>
2020-12-26 21:39:07 +0800

    Update get_time.sh

--- get_time.sh
+++ get_time.sh
@@ -1,7 +1,7 @@
 time=$(date "+%Y%m%d%H%M%S")
 echo "${time}" > ver
 git add ver
-git commit -m ${time} --author="github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>"
+git commit -m ${time} --allow-empty --author="github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>"
 #git tag v${time}
 #git push origin --tags
 git push
