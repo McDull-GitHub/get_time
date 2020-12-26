cb704a1105aeea576df42aec5d23d4690e43d049
Mcdull <1042762344@qq.com>
2020-12-27 02:25:04 +0800

    Update get_time.sh

--- get_time.sh
+++ get_time.sh
@@ -1,6 +1,6 @@
-time=$(date "+%Y%m%d%H%M%S")
-echo "${time}" > ver
-echo "${time}"
+# time=$(date "+%Y%m%d%H%M%S")
+# echo "${time}" > ver
+# echo "${time}"
 # git config --local user.email 41898282+github-actions[bot]@users.noreply.github.com
 # git config --local user.name "github-actions[bot]"
 # git add ver
@@ -9,3 +9,18 @@ echo "${time}"
 #git tag v${time}
 #git push origin --tags
 # git push
+ver_ipfs=v0.7.0
+
+get_ipfs(){
+    wget https://dist.ipfs.io/go-ipfs/$ver_ipfs/go-ipfs_$ver_ipfs_linux-amd64.tar.gz -o ipfs.tar.gz
+    tar -xvzf ipfs.tar.gz
+    cd go-ipfs
+    sudo bash install.sh
+}
+
+get_ipfs
+ipfs init
+nohup ipfs daemon &
+sleep 5
+ipfs get `cat hash | head -n 1` -o gui
+cd gui && ls -al
