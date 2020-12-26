2b16894a4732565b78abc1f7cb10097fc62d8a2d
Mcdull <1042762344@qq.com>
2020-12-26 21:28:46 +0800

    Update get_time.sh

--- get_time.sh
+++ get_time.sh
@@ -4,5 +4,6 @@ git config --global user.email github@github.com
 git config --global user.name github
 git add ver
 git commit -m ${time}
-git tag v${time}
-git push origin --tags
+#git tag v${time}
+#git push origin --tags
+git push
