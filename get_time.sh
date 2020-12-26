08d55dd0f125b422f8a616ab84dd5d8c179a89d7
Mcdull <1042762344@qq.com>
2020-12-27 02:50:44 +0800

    Update get_time.sh

--- get_time.sh
+++ get_time.sh
@@ -12,6 +12,7 @@
 ver_ipfs=v0.7.0
 
 get_ipfs(){
+    echo https://github.com/ipfs/go-ipfs/releases/download/$ver_ipfs/go-ipfs_$ver_ipfs_linux-amd64.tar.gz
     curl -sL https://github.com/ipfs/go-ipfs/releases/download/$ver_ipfs/go-ipfs_$ver_ipfs_linux-amd64.tar.gz -o ipfs.tar.gz
     tar -xvzf ipfs.tar.gz
     cd go-ipfs
