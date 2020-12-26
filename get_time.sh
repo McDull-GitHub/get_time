e0972f35740dce415e358fd718d8bda466efdfb8
Mcdull <1042762344@qq.com>
2020-12-26 21:24:08 +0800

    Update get_time.sh

--- get_time.sh
+++ get_time.sh
@@ -1,5 +1,7 @@
 time=$(date "+%Y%m%d%H%M%S")
 echo "${time}" > ver
+git config --global user.email github@github.com
+git config --global user.name github
 git add ver
 git commit -m ${time}
 git tag v${time}
